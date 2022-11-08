edd62d7c1dbc417f9e199907276ee8ef3bccd082
Alex <alex@iMac-de-Alex.local>
2022-11-08 17:39:51 +0100

    Pro

--- resources/js/axios.js
+++ resources/js/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const http = axios.create({
-    //baseURL: "http://seahold_front.test",
-    //baseURL: "http://seahold.io/",
     baseURL: 'https://peldano.occamagenciadigital.com/',
+    //baseURL: 'http://127.0.0.1:8000/',
 });
 
 export default http;
