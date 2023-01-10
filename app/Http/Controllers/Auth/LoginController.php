9316988589b36b7086fcbca9fc87e0901d00bd09
Alex <alex@iMac-de-Alex.local>
2023-01-10 12:39:20 +0100

    Añadido librería de login para la api y login hecho

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use JWTAuth;
+use Illuminate\Http\Request;
+use JWTAuthException;
+use Auth;
+use App\Models\User;
 
 class LoginController extends Controller
 {
@@ -42,5 +47,49 @@ class LoginController extends Controller
     {
         // your code
         return redirect()->intended($this->redirectTo); // add this line on the end of authenticated
+    }
+
+    //API
+    /**
+     * Login a user instance by api
+     *
+     * @param Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function loginapi(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        $token = null;
+        try {
+           if (!$token = JWTAuth::attempt($credentials)) {
+               $code = 1001;
+               $response['code'] = $code;
+               $response['message'] = 'Invalid email or password';
+               return response()->json($response);
+
+            
+           }
+        } catch (JWTAuthException $e) {
+            $code = 1002;
+            $response['code'] = $code;
+            $response['message'] = 'Failed to create token';
+            return response()->json($response);
+        }
+
+        $email = $request->get('email');
+        $user = User::where('email', $email)->first();
+
+        if(!$user){
+            $response['code'] = 1001;
+            return response()->json($response);  
+        }
+
+        $code = 1000;
+        $response['code'] = $code;
+        $response['message'] = 'Logged!';
+        $response['user_id'] = $user->id;
+        $response['token'] = compact('token')['token'];
+        return response()->json($response);
     }
 }
---
+++ app/Http/Middleware/VerifyJWTToken.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use JWTAuth;
+use Exception;
+use PHPOpenSourceSaver\JWTAuth\Http\Middleware\BaseMiddleware;
+
+class VerifyJWTToken extends BaseMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+    */
+    public function handle($request, Closure $next)
+    {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch (Exception $e) {
+            return $e;
+            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
+                $code = 1010;
+                $response['code'] = $code;
+                return response()->json($response);
+
+            }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
+                $code = 1010;
+                $response['code'] = $code;
+                return response()->json($response);
+                
+            }else{
+                return response()->json(['status' => 'Authorization Token not found']);
+            }
+        }
+        return $next($request);
+    }
+}
