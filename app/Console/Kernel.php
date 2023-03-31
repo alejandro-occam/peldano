89b6d0390441afbd0a35e74c1c8329fce5524280
Alejandro <alejandro.alvarez@occamcomunicacion.com>
2023-03-31 14:45:22 +0200

    Cambio de tiempo al renovar el token de Sage

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         //$schedule->command('createOrder:cron')->daily();
         $schedule->command('knowIfBillIsPay:cron')->daily();
-        $schedule->command('refreshTokenSage:cron')->everySixHours();
+        $schedule->command('refreshTokenSage:cron')->everyFourHours();
     }
 
     /**
